d91ffe2ae3261fed72314c341ecf8aea5c4df191
Santiago González <oscarsantiagogonzalezforero@gmail.com>
2025-02-09 21:27:51 -0500

    Creando la función para agregar amigos y la otra función que limpia el campo de entrada

--- app.js
+++ app.js
@@ -1,1 +1,22 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = [];
+
+function agregarAmigo() {
+    let nombre = document.getElementById('amigo').value;
+    console.log(nombre);
+
+    if(nombre == '') {
+        alert('Por favor ingresa un nombre, completando el campo.');
+    } else {
+        nombre = amigos.push(document.getElementById('amigo').value);
+        limpiarCampoEntrada();
+    }
+
+    console.log(amigos);
+
+    return;
+}
+
+function limpiarCampoEntrada() {
+    document.getElementById('amigo').value = '';
+}
