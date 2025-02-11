1457975795d7133cd06cab3155e548f2beacf75e
Santiago González <oscarsantiagogonzalezforero@gmail.com>
2025-02-10 20:56:23 -0500

    Función que muestra los nombres de los amigos con un ciclo for

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 let amigos = [];
 
+
+
 function agregarAmigo() {
     let nombre = document.getElementById('amigo').value;
     console.log(nombre);
@@ -10,6 +12,7 @@ function agregarAmigo() {
     } else {
         nombre = amigos.push(document.getElementById('amigo').value);
         limpiarCampoEntrada();
+        mostrarAmigos();
     }
 
     console.log(amigos);
@@ -17,6 +20,19 @@ function agregarAmigo() {
     return;
 }
 
+function mostrarAmigos() {
+    let listaAmigos = document.getElementById('listaAmigos');
+    listaAmigos.innerHTML = '';
+    console.log(listaAmigos);
+
+    for (let i = 0; i < amigos.length; i++) {
+        listaAmigos.innerHTML += `<li>${amigos[i]}</li>`;
+    }
+
+    return;
+}
+
 function limpiarCampoEntrada() {
     document.getElementById('amigo').value = '';
 }
+
